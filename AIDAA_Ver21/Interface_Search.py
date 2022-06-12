d2aa2aa2ff727d68a17980711368dbe444fdf136
parkjihun2992 <jihun2992@naver.com>
2022-06-12 16:12:10 +0900

    Main 화면 오른쪽 - 비정상 절차서 검색 - 시스템 검색 클릭시 팝업창 활성화

---
+++ AIDAA_Ver21/Interface_Search.py
@@ -0,0 +1,54 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from AIDAA_Ver21.Mem_ShMem import ShMem, InterfaceMem
+from AIDAA_Ver21.Interface_ABCWidget import *
+from AIDAA_Ver21.Simulator_CNS import *
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+class ProcedureSearch(ABCWidget, QWidget):
+    def __init__(self, parent):
+        super(ProcedureSearch, self).__init__(parent)
+        self.setStyleSheet('background-color: rgb(212, 245, 211);')
+        self.setGeometry(200, 200, 390, 300)
+        lay = QVBoxLayout(self)
+        lay.addWidget(ProcedureSearchWindow(self))
+
+class ProcedureSearchWindow(ABCWidget, QWidget):
+    def __init__(self, parent):
+        super(ProcedureSearchWindow, self).__init__(parent)
+        lay = QVBoxLayout(self)
+        lay.addWidget(ProcedureSearchKeyword(self))
+        lay.addWidget(ProcedureSearchNumber(self))
+
+class ProcedureSearchKeyword(ABCLabel, QLabel):
+    def __init__(self, parent):
+        super(ProcedureSearchKeyword, self).__init__(parent)
+        self.setText('키워드')
+
+class ProcedureSearchNumber(ABCLabel, QLabel):
+    def __init__(self, parent):
+        super(ProcedureSearchNumber, self).__init__(parent)
+        self.setText('절차서 번호')
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+class SystemSearch(ABCWidget, QWidget):
+    def __init__(self, parent):
+        super(SystemSearch, self).__init__(parent)
+        self.setStyleSheet('background-color: rgb(212, 245, 211);')
+        self.setGeometry(200, 200, 390, 300)
+        lay = QVBoxLayout(self)
+        lay.addWidget(SystemSearchWindow(self))
+
+class SystemSearchWindow(ABCWidget, QWidget):
+    def __init__(self, parent):
+        super(SystemSearchWindow, self).__init__(parent)
+        lay = QVBoxLayout(self)
+        lay.addWidget(SystemSearchName(self))
+
+class SystemSearchName(ABCLabel, QLabel):
+    def __init__(self, parent):
+        super(SystemSearchName, self).__init__(parent)
+        self.setText('시스템')
