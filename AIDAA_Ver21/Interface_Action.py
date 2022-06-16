902da0d8954d8d6c0e9aaeeed77cb1f2d3f2b25f
DaeilLee <dleodlf1004@naver.com>
2022-06-16 21:55:30 +0900

    Action 인터페이스 - RubberBand - 방향키 사용 item 이동 - pipe, non-item 추가 - csv 파일 사용한 db 관리 기능 추가

--- AIDAA_Ver21/Interface_Action.py
+++ AIDAA_Ver21/Interface_Action.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtSvg import QGraphicsSvgItem, QSvgRenderer
+import pandas as pd
 from AIDAA_Ver21.Mem_ShMem import ShMem, InterfaceMem
 from AIDAA_Ver21.Interface_ABCWidget import *
 
@@ -42,6 +43,7 @@ class Action_system_mimic_area(ABCWidget):
         self.Action_system_scene_ = Action_system_scene(self)
         self.Action_system_view_ = QGraphicsView(self.Action_system_scene_, self)
         self.Action_system_view_.setStyleSheet('border: 0px')
+        self.Action_system_view_.setDragMode(QGraphicsView.DragMode.RubberBandDrag)
         lay.addWidget(self.Action_system_view_)
 
         # ---------------------------------
@@ -49,7 +51,10 @@ class Action_system_mimic_area(ABCWidget):
         # ---------------------------------
         self.shortcut_save_scene = QShortcut(QKeySequence('Ctrl+S'), self, self.inmem.widget_ids['Action_system_scene'].save_scene)
         self.shortcut_load_scene = QShortcut(QKeySequence('Ctrl+L'), self, self.inmem.widget_ids['Action_system_scene'].load_scene)
-        self.shortcut_move_item_up = QShortcut(Qt.Key.Key_Up, self, self.inmem.widget_ids['Action_system_scene'].move_item_up)
+        self.shortcut_move_item_up = QShortcut(Qt.Key.Key_Up, self, lambda: self.inmem.widget_ids['Action_system_scene'].move_item('up'))
+        self.shortcut_move_item_up = QShortcut(Qt.Key.Key_Down, self, lambda: self.inmem.widget_ids['Action_system_scene'].move_item('down'))
+        self.shortcut_move_item_up = QShortcut(Qt.Key.Key_Right, self, lambda: self.inmem.widget_ids['Action_system_scene'].move_item('right'))
+        self.shortcut_move_item_up = QShortcut(Qt.Key.Key_Left, self, lambda: self.inmem.widget_ids['Action_system_scene'].move_item('left'))
 
     def resizeEvent(self, a0) -> None:
         w, h = self.Action_system_view_.size().width(), self.Action_system_view_.size().height()
@@ -64,6 +69,7 @@ class Action_system_scene(ABCGraphicsScene):
         super(Action_system_scene, self).__init__(parent)
         self.setBackgroundBrush(QColor(231, 231, 234))
         self.svg_render = QSvgRenderer('./CVCS/Core_comp.svg')
+        self.load_scene()
 
     def save_scene(self):
         """
@@ -75,32 +81,122 @@ class Action_system_scene(ABCGraphicsScene):
         """
         저장 화면 불러오기 (Ctrl+L)
         """
+        # If any items is added in scene, the items will be removed.
+        for item in self.items():
+            self.removeItem(item)
+        # Draw
+        self._update_line() # Update the pipes
+        self._update_nonitem() # Update the non-item components
         print('화면 로딩')
 
-    def move_item_up(self):
-        """
-        선택된 아이템이 있는 경우 Up
-        """
-        print('아이템 Up')
+    def move_item(self, direction):
+        # If one or more than one item is selected and Up key is pressed, the item selected will be moved up, down, right or left.
+        for item in self.selectedItems():
+            if direction == 'up':
+                item.move_pos(0, -1)
+            if direction == 'down':
+                item.move_pos(0, 1)
+            if direction == 'right':
+                item.move_pos(1, 0)
+            if direction == 'left':
+                item.move_pos(-1, 0)
 
     def _update_line(self):
-        with open('./CVCS/Core_pip_info.csv', 'r') as f:
-            info = f.read().split('\n')
-            for info_item in info:
-                if info_item != '':
-                    info_line = info_item.split(',')
-                    if info_line[1] == 'PIP':
-                        self.addItem(PipLine(QLineF(float(info_line[2]), float(info_line[3]),
-                                                    float(info_line[4]), float(info_line[5])),
-                                             QPen(Qt.black, 3),
-                                             int(info_line[6])))
-            for info_item in info:
-                if info_item != '':
-                    info_line = info_item.split(',')
-                    if info_line[1] != 'PIP':
-                        self.addItem(SVGnonitem(float(info_line[2]), float(info_line[3]),
-                                                info_line[1],
-                                                self.svg_render,
-                                                info_line[4],
-                                                info_line[5],
-                                                info_line[6]))
+        # Load the pipe data from the CSV file
+        info_ = pd.read_csv('./CVCS/Core_pip_info.csv')
+        # Draw the pipe
+        for idx, row in info_.iterrows():
+            self.addItem(Pipeline(
+                line=QLineF(float(row['X1']), float(row['Y1']), float(row['X2']), float(row['Y2'])), 
+                pen=QPen(Qt.black, 3),
+                arrow_type=int(row['Opt'])))
+
+    def _update_nonitem(self):
+        # Load the non-item component data from the CSV file
+        info_ = pd.read_csv('./CVCS/Core_nonitem_info.csv')
+        # Draw the non-item component
+        for idx, row in info_.iterrows():
+            self.addItem(SVGnonitem(
+                x=float(row['X1']), y=float(row['Y1']), id=row['Type'], render=self.svg_render, 
+                rota=row['Rota'], scal=row['Scal'], comment=row['Comment']
+                ))
+
+class Pipeline(QGraphicsLineItem):
+    def __init__(self, line: QLineF, pen, arrow_type=0):
+        self.line_info = line
+        self.arrow_type = arrow_type
+
+        super(Pipeline, self).__init__(line)
+
+        self.setPen(pen)
+
+        self.head_arrow = QGraphicsPolygonItem(QPolygonF([]), self)
+        self.update_shape()
+
+        self.setFlag(QGraphicsItem.ItemIsSelectable, True)
+        self.setFlag(QGraphicsItem.ItemIsMovable, True)
+
+        self.selected_line_pos = None
+        self.selected_line = [False, False]
+
+    def update_shape(self):
+        if self.arrow_type == 0:
+            arrow_points = QPolygonF([])
+            transform = QTransform()
+        elif self.arrow_type == 1:
+            # Polygon
+            arrow_points = QPolygonF([
+                QPointF(self.line().p1().x() - 7, self.line().p1().y()),
+                QPointF(self.line().p1().x() + 7, self.line().p1().y()),
+                QPointF(self.line().p1().x(), self.line().p1().y() + 7),
+            ])
+            #
+            transform = QTransform()
+            transform.translate(self.line().p1().x(), self.line().p1().y())
+            transform.rotate(- self.line().angle() + 90)
+            transform.translate(-self.line().p1().x(), -self.line().p1().y())
+        elif self.arrow_type == 2:
+            # Polygon
+            arrow_points = QPolygonF([
+                QPointF(self.line().p2().x() - 7, self.line().p2().y()),
+                QPointF(self.line().p2().x() + 7, self.line().p2().y()),
+                QPointF(self.line().p2().x(), self.line().p2().y() + 7),
+            ])
+            #
+            transform = QTransform()
+            transform.translate(self.line().p2().x(), self.line().p2().y())
+            transform.rotate(- self.line().angle() - 90)
+            transform.translate(-self.line().p2().x(), -self.line().p2().y())
+
+        self.head_arrow.setPolygon(arrow_points)
+        self.head_arrow.setBrush(QBrush(Qt.black))
+        self.head_arrow.setPen(QPen(Qt.black, 1))
+
+        self.head_arrow.setTransform(transform)
+        
+    def move_pos(self, x, y):
+        self.setLine(self.line().translated(x, y))
+        self.update_shape()
+
+class SVGnonitem(QGraphicsSvgItem):
+    def __init__(self, x, y, id: str, render, rota, scal, comment, *args):
+        super(SVGnonitem, self).__init__(*args)
+        self.setSharedRenderer(render)
+        self.setElementId(id)
+        self.setX(x)
+        self.setY(y)
+        self.setFlag(QGraphicsItem.ItemIsMovable, True)
+        self.setFlag(QGraphicsItem.ItemIsSelectable, True)
+        self.setRotation(float(rota))
+        self.setScale(float(scal))
+        self.comment = comment
+
+    # def mousePressEvent(self, event: 'QGraphicsSceneMouseEvent') -> None:
+    #     super(SVGnonitem, self).mousePressEvent(event)
+    #     if event.buttons() == Qt.LeftButton:
+    #         print(f'Get Press {self.pos().x()}, {self.pos().y()}')
+    #         print(f'{self.boundingRect()}')
+
+    def move_pos(self, x=0, y=0):
+        self.setX(self.x() + x)
+        self.setY(self.y() + y)
