029d3cd6bcbd24a6cd2a2c00a8f35001787b4bca
parkjihun2992 <jihun2992@naver.com>
2022-07-20 11:10:47 +0900

    # 6756ee70 cherry pick

--- AIDAA_Ver21/Function_Mem_ShMem.py
+++ AIDAA_Ver21/Function_Mem_ShMem.py
@@ -1,5 +1,4 @@
 from AIDAA_Ver21.DB_AlarmDB import AlarmDB
-from collections import deque
 from datetime import timedelta
 from AIDAA_Ver21.ab_procedure import ab_pro
 from collections import deque
@@ -92,16 +91,14 @@ class ShMem:
 
     # Procedure
     def get_pro_procedure(self, procedure_name):
-        return {'경보 및 증상': ab_pro[procedure_name]['경보 및 증상'], '자동 동작 사항': ab_pro[procedure_name]['자동 동작 사항'], '긴급 조치 사항': ab_pro[procedure_name]['긴급 조치 사항'], '후속 조치 사항': ab_pro[procedure_name]['후속 조치 사항']}
+        return {'경보 및 증상': ab_pro[procedure_name]['경보 및 증상'], '자동 동작 사항': ab_pro[procedure_name]['자동 동작 사항'],
+                '긴급 조치 사항': ab_pro[procedure_name]['긴급 조치 사항'], '후속 조치 사항': ab_pro[procedure_name]['후속 조치 사항']}
 
     def get_pro_procedure_count(self, procedure_name):
-        return {'경보 및 증상': len(ab_pro[procedure_name]['경보 및 증상'].keys()), '자동 동작 사항': len(ab_pro[procedure_name]['자동 동작 사항'].keys()), '긴급 조치 사항': len(ab_pro[procedure_name]['긴급 조치 사항'].keys()), '후속 조치 사항': len(ab_pro[procedure_name]['후속 조치 사항'].keys())}
         return {'목적': 0, '경보 및 증상': len(ab_pro[procedure_name]['경보 및 증상'].keys()),
                 '자동 동작 사항': len(ab_pro[procedure_name]['자동 동작 사항'].keys()),
                 '긴급 조치 사항': len(ab_pro[procedure_name]['긴급 조치 사항'].keys()),
                 '후속 조치 사항': len(ab_pro[procedure_name]['후속 조치 사항'].keys())}
-
-
 # ----------------------------------------------------------------------------------------------------------------------
 
 
@@ -116,19 +113,19 @@ class InterfaceMem:
         self.system_state_switch = {'Normal': 1, 'Pre-abnormal': 0, 'Abnormal': 0, 'Emergency': 0}
 
         self.diagnosis_convert_text = {0: 'Normal: 정상', 1: 'Ab21_01: 가압기 압력 채널 고장 (고)', 2: 'Ab21_02: 가압기 압력 채널 고장 (저)',
-                                  3: 'Ab20_04: 가압기 수위 채널 고장 (저)', 4: 'Ab15_07: 증기발생기 수위 채널 고장 (저)',
-                                  5: 'Ab15_08: 증기발생기 수위 채널 고장 (고)',
-                                  6: 'Ab63_04: 제어봉 낙하', 7: 'Ab63_02: 제어봉의 계속적인 삽입', 8: 'Ab21_12: 가압기 PORV (열림)',
-                                  9: 'Ab19_02: 가압기 안전밸브 고장', 10: 'Ab21_11: 가압기 살수밸브 고장 (열림)',
-                                  11: 'Ab23_03: CVCS에서 1차기기 냉각수 계통(CCW)으로 누설',
-                                  12: 'Ab60_02: 재생열교환기 전단부위 파열', 13: 'Ab59_02: 충전수 유량조절밸즈 후단누설',
-                                  14: 'Ab23_01: RCS에서 1차기기 냉각수 계통(CCW)으로 누설', 15: 'Ab23_06: 증기발생기 전열관 누설',
-                                  16: '해당 시나리오는 학습되지 않은 시나리오입니다.', 17: '학습여부를 아직 확인할 수 없습니다.'}
+                                       3: 'Ab20_04: 가압기 수위 채널 고장 (저)', 4: 'Ab15_07: 증기발생기 수위 채널 고장 (저)',
+                                       5: 'Ab15_08: 증기발생기 수위 채널 고장 (고)',
+                                       6: 'Ab63_04: 제어봉 낙하', 7: 'Ab63_02: 제어봉의 계속적인 삽입', 8: 'Ab21_12: 가압기 PORV (열림)',
+                                       9: 'Ab19_02: 가압기 안전밸브 고장', 10: 'Ab21_11: 가압기 살수밸브 고장 (열림)',
+                                       11: 'Ab23_03: CVCS에서 1차기기 냉각수 계통(CCW)으로 누설',
+                                       12: 'Ab60_02: 재생열교환기 전단부위 파열', 13: 'Ab59_02: 충전수 유량조절밸즈 후단누설',
+                                       14: 'Ab23_01: RCS에서 1차기기 냉각수 계통(CCW)으로 누설', 15: 'Ab23_06: 증기발생기 전열관 누설',
+                                       16: '해당 시나리오는 학습되지 않은 시나리오입니다.', 17: '학습여부를 아직 확인할 수 없습니다.'}
         self.current_procedure = {self.diagnosis_convert_text[i]: {'num': 0, 'des': {0: '내용 없음', 1: '목적', 2: '경보 및 증상',
                                                                                      3: '자동 동작 사항', 4: '긴급 조치 사항',
                                                                                      5: '후속 조치 사항'}} for i in range(18)}
         self.current_procedure_log = [0, 0]  # [절차서 화면 전환 용도, 선택 절차서 전환 용도]
-        self.current_table = {'Procedure':0, 'System': 0, 'current_window': -1, 'procedure_name':""}
+        self.current_table = {'Procedure': 0, 'System': 0, 'current_window': -1, 'procedure_name': ""}
 
         self.procedure_progress_state = {
             self.diagnosis_convert_text[i]: {'목적': 0, '경보 및 증상': 0, '자동 동작 사항': 0, '긴급 조치 사항': 0, '후속 조치 사항': 0} for i
@@ -151,28 +148,30 @@ class InterfaceMem:
         self.diagnosis_para = pd.read_csv('./AI/Final_parameter_200825.csv')['0'].tolist()
         self.train_check_para = pd.read_csv('./AI/Final_parameter.csv')['0'].tolist()
         self.diagnosis_model = pickle.load(open('./AI/Ab_Diagnosis_model.h5', 'rb'))
-        self.train_check_model = load_model('./AI/Train_Untrain_epoch27_[0.00225299]_acc_[0.9724685967462512].h5', compile=False)
+        self.train_check_model = load_model('./AI/Train_Untrain_epoch27_[0.00225299]_acc_[0.9724685967462512].h5',
+                                            compile=False)
         print('인공지능 모델 로드 완료')
 
-        self.dis_AI = {'AI': '', 'Train':''}
+        self.dis_AI = {'AI': '', 'Train': ''}
         self.dis_AI_system = [['CVCS', '03/09', '72%']]
-
 
     # 인공지능 전처리 용 ------------------------------------------------------------------------------------------------------
     def get_diagnosis_val(self):
         return [self.ShMem.get_para_val(i) for i in self.diagnosis_para]
 
     def get_diagnosis_result(self):  # 상위 5개의 진단 결과만 출력
-        self.dis_AI['AI'] = [self.make_raw(max_v, max_i) for (max_v, max_i) in self.GetTop(self.diagnosis_model.predict([self.get_diagnosis_val()])[0], 5)]
+        self.dis_AI['AI'] = [self.make_raw(max_v, max_i) for (max_v, max_i) in
+                             self.GetTop(self.diagnosis_model.predict([self.get_diagnosis_val()])[0], 5)]
 
     def get_train_check_val(self):
-        return np.array([np.array([self.ShMem.get_para_list(i) for i in self.train_check_para]).reshape(-1,46)])
+        return np.array([np.array([self.ShMem.get_para_list(i) for i in self.train_check_para]).reshape(-1, 46)])
 
-    def get_train_check_result(self): # 데이터 shape: (1,10,46) 강제
-        if np.mean(np.power(self.flatten(self.get_train_check_val())-self.flatten(self.train_check_model.predict(self.get_train_check_val())), 2), axis=1)[0] <= 0.00225299:
-            self.dis_AI['Train'] = 0 # 훈련된 시나리오
+    def get_train_check_result(self):  # 데이터 shape: (1,10,46) 강제
+        if np.mean(np.power(self.flatten(self.get_train_check_val()) - self.flatten(
+                self.train_check_model.predict(self.get_train_check_val())), 2), axis=1)[0] <= 0.00225299:
+            self.dis_AI['Train'] = 0  # 훈련된 시나리오
         else:
-            self.dis_AI['Train'] = 1 # 훈련되지 않은 시나리오
+            self.dis_AI['Train'] = 1  # 훈련되지 않은 시나리오
 
     def flatten(self, X):
         '''
@@ -209,7 +208,8 @@ class InterfaceMem:
             radiation = False
             total_symptomc = 0
             total_symptoms = 0
-        return [procedure_name, urgent_action, radiation, f'{total_symptoms:02}/{total_symptomc:02}', f'{max_v * 100:2.2f}%']
+        return [procedure_name, urgent_action, radiation, f'{total_symptoms:02}/{total_symptomc:02}',
+                f'{max_v * 100:2.2f}%']
 
     def GetTop(self, raw_list, get_top):
         """ 리스트에서 최대값 랭크와 인덱스 제공 """
